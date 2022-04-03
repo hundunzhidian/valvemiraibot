4468cd0f06d2381f2f3a3e8aa3d0e9f1dc8ff710
QAQ <1414290810@qq.com>
2022-04-03 18:59:11 +0800

    修复求生查询人数出现异常的问题 移除多余的代码

---
+++ src/test/java/cn/qaq/valveqqrobot/Test.java
@@ -0,0 +1,18 @@
+package cn.qaq.valveqqrobot;
+
+import cn.qaq.valveqqrobot.service.QQService;
+
+/**
+ * @program: valveqqrobot
+ * @description:
+ * @author: QAQ
+ * @create: 2022-04-03 18:45
+ **/
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void t1()
+    {
+//        QQService qqService=new QQService();
+//        System.out.println(qqService.getServerPlayers());
+    }
+}
